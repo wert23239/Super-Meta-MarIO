132778ede7490d65597613c5f3248c462c3643fd
wert23239 <lambe168@msu.edu>
2017-07-03 21:01:05 -0800

    Probalistic Protoype Completed but Untested

--- Main.py
+++ Main.py
@@ -17,17 +17,33 @@ from lib.SQL import SQLCalls
 import sqlite3
 
 SQL=SQLCalls()
-SQL.GatherGenomes()
-exit()
+
+
 def process_img(original_image):
     processed_img=cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
     processed_img= cv2.resize(processed_img,(28,28))
     return processed_img
 
+def do_action(SQL,frame_count):
+    print_screen = np.array(ImageGrab.grab(bbox=(0,60,580,530)))
+    new_screen=np.array(np.reshape(process_img(print_screen),[28,28,1]))
+    #Gain Action from Tenserflow
+    a_dist = sess.run(myAgent.final_output,feed_dict={myAgent.genomes:Genomes,myAgent.state_in:[new_screen]\
+                                                             ,myAgent.used_genomes:UsedGenomes,myAgent.condition:0\
+                                                                 ,myAgent.action_holder:[10]})
+    a = np.random.choice(a_dist,p=a_dist)
+    a = np.argmax([a_dist] == a)
+
+    print("update "+ str(frame_count))
+    SQL.update_table(new_screen,int(a+1)) 
+    print("update completed")
+    frame_count+=1
+    return frame_count
+
 
 #Pre Tenserflow Session Setup
 tf.reset_default_graph()
-myAgent = agent(lr=1e-2,s_size=28*28,a_size=4,h_size=64)
+myAgent = agent(lr=1e-2,s_size=28,a_size=4,h_size=64)
 init = tf.global_variables_initializer()
 update_frequency = 5
 
@@ -39,22 +55,8 @@ ACTION,WAIT,DEATH=0,1,2
 while SQL.check_table()==WAIT:
     pass
 print("Ready!")   
-
-def do_action(SQL,frame_count):
-    print_screen = np.array(ImageGrab.grab(bbox=(0,60,580,530)))
-    new_screen=np.array(np.reshape(process_img(print_screen),[-1]))
-    #Gain Action from Tenserflow
-    a_dist = sess.run(myAgent.output,feed_dict={myAgent.state_in:[new_screen]})
-    a = np.random.choice(a_dist[0],p=a_dist[0])
-    a = np.argmax(a_dist == a)
-    rand_num=random.randint(0,10)
-    if rand_num>=0:
-        a=5
-    print("update "+ str(frame_count))
-    SQL.update_table(new_screen,int(a+1)) 
-    print("update completed")
-    frame_count+=1
-    return frame_count
+Genomes=SQL.GatherGenomes()
+UsedGenomes=np.ones(Genomes.shape[0])
 
 with tf.Session() as sess: 
 
@@ -76,6 +78,7 @@ with tf.Session() as sess:
             frame_count=do_action(SQL,frame_count)
         elif check==DEATH: #Mario has Died
             print("DEATH")
+            break
             ep_history =SQL.gain_history()
             ep_history[:,2] = discount_rewards(ep_history[:,2])
             feed_dict={myAgent.reward_holder:ep_history[:,2],
