Fixed Sigmoid Overflow error

Main.py
@@ -53,7 +53,11 @@ def do_action(SQL,frame_count):
     f_dict={mainQN.used_genomes:UsedGenomes,mainQN.genomes:Genomes,\
     mainQN.imageIn:[new_screen],mainQN.condition:0,mainQN.correct_action:[10],mainQN.correct_mean:[10]}
     #Gain Action from Tenserflow
-    a,before = sess.run([mainQN.predict,mainQN.Smooth],feed_dict=f_dict)
+    a,value,out = sess.run([mainQN.predict,mainQN.Value,mainQN.Qout],feed_dict=f_dict)
+    if(a==0):
+        print(a)
+        print(value)
+        print(out)
     #a = np.random.choice(a_dist,p=a_dist)
     #a = np.argmax([a_dist] == a)
     #print(a)
@@ -189,7 +193,7 @@ with tf.Session() as sess:
                         mainQN.imageIn:states,mainQN.condition:1,
                         mainQN.correct_action:trainBatch[:,1],mainQN.correct_mean:np.hstack(mean_list),
                        mainQN.targetQ:targetQ}
-            _,loss =sess.run([mainQN.updateModel,mainQN.loss], \
+            _,loss,Qs =sess.run([mainQN.updateModel,mainQN.loss,mainQN.Qout], \
                 feed_dict=final_dict)
             updateTarget(targetOps,sess)
             print("Epoch " + str(epoch) + " Complete")

lib/reinforcement.py
@@ -51,7 +51,7 @@ def updateTarget(op_holder,sess):
 
 class Qnetwork():
     def __init__(self,h_size,s_size,POPULATION,BATCH,myScope):
-        self.genomes= tf.placeholder(shape=[POPULATION,30,3],dtype=tf.int32)   
+        self.genomes= tf.placeholder(shape=[POPULATION,300,3],dtype=tf.int32)   
         self.condition = tf.placeholder(tf.int32, shape=[], name="condition")
         self.correct_action=tf.placeholder(shape=[None], dtype=tf.int32)
         self.correct_mean=tf.placeholder(shape=[None], dtype=tf.float32)
@@ -89,7 +89,7 @@ class Qnetwork():
         #hidden_rnn=tf.cond(self.condition < 1, lambda: hidden_rnn,lambda: tf.gather(hidden_rnn,self.action_holder)) 
         self.hidden_combined= slim.fully_connected(combined,h_size,biases_initializer=None,activation_fn=tf.nn.relu)
         self.hidden_combined_2= slim.fully_connected(self.hidden_combined,h_size//4,biases_initializer=None,activation_fn=tf.nn.relu)
-        self.regression= slim.fully_connected(self.hidden_combined_2,1,biases_initializer=None,activation_fn=tf.nn.tanh)
+        self.regression= slim.fully_connected(self.hidden_combined_2,1,biases_initializer=None,activation_fn=None)
         self.Advantage=tf.cond(self.condition <  1, 
         lambda: tf.reshape(self.regression,[1,POPULATION]), 
         lambda: tf.reshape(self.regression,[1,BATCH])[0])